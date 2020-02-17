b7ed30431b9286ac0815b9793e6e029141a6a6a6
Leo Dion <leogdion@brightdigit.com>
2020-02-17 17:15:36 -0500

    adding user defaults view

---
+++ apiHarvesterApp/UserDefaultsView.swift
@@ -0,0 +1,21 @@
+//
+//  UserDefaultsView.swift
+//  apiHarvesterApp
+//
+//  Created by Leo Dion on 2/17/20.
+//  Copyright © 2020 BrightDigit. All rights reserved.
+//
+
+import SwiftUI
+
+struct UserDefaultsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct UserDefaultsView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserDefaultsView()
+    }
+}
