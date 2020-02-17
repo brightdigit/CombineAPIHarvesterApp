3ac6a7a4e325c200aa52e87cbfd067263c1eeb64
Leo Dion <leogdion@brightdigit.com>
2020-02-17 16:45:44 -0500

    Initial Commit

---
+++ apiHarvesterApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apiHarvesterApp
+//
+//  Created by Leo Dion on 2/17/20.
+//  Copyright © 2020 BrightDigit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
